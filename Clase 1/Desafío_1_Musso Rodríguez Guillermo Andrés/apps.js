823f64b50a02aab36fc277f13e38d2da9972860e
Guillermo A Musso Rodriguez <musso.guillermo.capacitacion@gmail.com>
2021-10-16 15:04:35 -0300

    16/10/2021

---
+++ Clase 1/Desafío_1_Musso Rodríguez Guillermo Andrés/apps.js
@@ -0,0 +1,17 @@
+/*Curso de Coderhouse: 23240
+Estudiante: Guillermo Andrés Musso Rodríguez
+e-Mail: gamussorodriguez@teco.com.ar
+Teléfono: 1130743868
+Fecha: 13/10/2021
+
+****Desafío Uno 1****
+
+*/
+let numeroA, numeroB, resultado;
+numeroA = Number(prompt("Ingrese un número"));
+console.log(numeroA);
+numeroB = Number(prompt("Ingrese un número a sumar"));
+console.log(numeroB);
+resultado = numeroA + numeroB;
+document.write("La operación reslizada fue:"+" "+ numeroA + " + " + numeroB + " " + "El resultado es" + " " + resultado);
+console.log(resultado);
