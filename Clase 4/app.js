8a5f07dce783f19f2d806b066e7ae96ee7a15ca6
Guillermo A Musso Rodriguez <musso.guillermo.capacitacion@gmail.com>
2021-10-28 01:00:28 -0300

    28/10/2021

--- Clase 4/app.js
+++ Clase 4/app.js
@@ -1,42 +1,74 @@
-//Clase 3: Ciclos/Iteraciones
+//Clase 4: Programación avanzada con Funciones
 console.log("Hola Mundo");
 
 /*Curso de Coderhouse: 23240
 Estudiante: Guillermo Andrés Musso Rodríguez
 e-Mail: gamussorodriguez@teco.com.ar
 Teléfono: 1130743868
-Fecha: 23/10/2021
+Fecha: 27/10/2021
 
-****Desafío 3 (Tres)****
+****Desafío 4 (Cuatro)****
 
 */
-let numeroA, numeroB, resultado;
-numeroA = prompt("Ingrese un número");
-while (isNaN(numeroA)) {
-    console.log(numeroA);
+
+const mensajeFlecha = (valor3) => alert(valor3);
+mensajeFlecha("¡Le damos la Bienvenida! Haga click en Aceptar para Continuar");
+
+function precioActual(costo,markUp,impuesto) {
+    let precioDeVenta = (costo + impuesto)*(1+(markUp/100));
+    return precioDeVenta;
+}
+//la variable "tiempo" es la cantidad de meses
+function actualizarPrecio(precio,inflacion,tiempo) {
+    tiempo = tiempo || 3;
+    let precioActualizado = (precio)*(1+inflacion*tiempo/100);
+    return precioActualizado;
+}
+
+let costoIngresado = prompt("Ingrese el costo sin impuestos");
+while (isNaN(costoIngresado)) {
+    console.log(costoIngresado);
     console.log("Valor inválido. Debe ingresar un número");
-    document.write("Usted ingresó:"+" "+ numeroA + "." + " " + "Debe ingresar un número");
+    document.write("<br>Usted ingresó:"+" "+ costoIngresado + "." + " " + "Debe ingresar un número");
     alert ("Debe ingresar un número");
-    numeroA = prompt("Ingrese un número");
+    costoIngresado = prompt("Ingrese un número");
 }
-numeroB = prompt("Ingrese número a sumar al número ingresado anteriormente");
-while (isNaN(numeroB)) {
-    console.log(numeroB);
+let impuestoIngresado = prompt("Ingrese el impuesto");
+while (isNaN(impuestoIngresado)) {
+    console.log(impuestoIngresado);
     console.log("Valor inválido. Debe ingresar un número");
-    document.write("Usted ingresó:"+" "+ numeroB + "." + " " + "Debe ingresar un número");
+    document.write("<br>Usted ingresó:"+" "+ impuestoIngresado + "." + " " + "Debe ingresar un número");
     alert ("Debe ingresar un número");
-    numeroB = prompt("Ingrese número a sumar al número ingresado anteriormente");
+    impuestoIngresado = prompt("Ingrese un número");
 }
-resultado = Number(numeroA) + Number(numeroB);
-console.log(resultado);
-console.log("El resultado es " + resultado);
-document.write("El resultado de la operación:"+" "+ numeroA + "+" + numeroB + " es " + resultado);
-alert ("El resultado de la operación:"+" "+ numeroA + "+" + numeroB + " es " + resultado);
-console.log("A continuación se decrementará el resultado " + resultado + " hasta llegar a 0");
-document.write("<br>A continuación se decrementará el resultado " + resultado + " hasta llegar a 0:");
-let res = resultado;
-for (let i = 0; i < res; i++) {
-    resultado = resultado-1;
-    console.log("\n" + resultado);
-    document.write("<br>" + resultado);
+let markUpIngresado = prompt("Ingrese el MarkUp deseado");
+while (isNaN(markUpIngresado)) {
+    console.log(markUpIngresado);
+    console.log("Valor inválido. Debe ingresar un número");
+    document.write("<br>Usted ingresó:"+" "+ markUpIngresado + "." + " " + "Debe ingresar un número");
+    alert ("Debe ingresar un número");
+    markUpIngresado = prompt("Ingrese un número");
 }
+let precioHoy = precioActual(Number(costoIngresado),Number(markUpIngresado),Number(impuestoIngresado));
+console.log(precioActual);
+document.write("<br>El precio actual de venta es:"+" "+ precioHoy);
+alert (precioHoy);
+let inflacion = prompt("ingrese el valor en % de inflacion mensual");
+while (isNaN(inflacion)) {
+    console.log(inflacion);
+    console.log("Valor inválido. Debe ingresar un número");
+    document.write("Usted ingresó:"+" "+ inflacion + "." + " " + "Debe ingresar un número");
+    alert ("Debe ingresar un número");
+    inflacion = prompt("Ingrese un número");
+}
+let mensual = prompt("ingrese la cantidad de meses a proyectar");
+while (isNaN(mensual)) {
+    console.log(mensual);
+    console.log("Valor inválido. Debe ingresar un número");
+    document.write("<br>Usted ingresó:"+" "+ mensual + "." + " " + "Debe ingresar un número");
+    alert ("Debe ingresar un número");
+    mensual = prompt("Ingrese un número");
+}
+let precioActualizado = actualizarPrecio(precioHoy,inflacion,mensual);
+console.log(precioActualizado);
+document.write("<br>El preion Actualizado en"+" "+ mensual + " meses " + "será de " + precioActualizado);
